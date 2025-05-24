2cc7d65e1cf25056bb3cdf5fec7285a98bee57ef
ALASSANE Fadeilou <68302531+Fadeilou@users.noreply.github.com>
2025-05-24 19:47:53 +0100

    Create crud.js

---
+++ 0x04-TypeScript/task_3/js/crud.js
@@ -0,0 +1,16 @@
+// This file is provided by the project.
+// Ensure it exists at this path.
+export function insertRow(row) {
+  console.log('Insert row', row);
+  return Math.floor(Math.random() * Math.floor(1000));
+}
+
+export function deleteRow(rowId) {
+  console.log('Delete row id', rowId);
+  return;
+}
+
+export function updateRow(rowId, row) {
+  console.log(`Update row ${rowId}`, row);
+  return rowId;
+}
